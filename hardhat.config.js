a07cd8d385601542fd56d2044af937c5e5933e73
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-03-18 20:40:22 +0100

    Add NFT contract and finished project configuration.

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+// require("@nomiclabs/hardhat-waffle");
+
+require('dotenv').config()
+require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-ethers')
+const {API_KEY, PRIVATE_KEY, ETHERSCAN_API_KEY} = process.env;
+
+
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2${API_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2${API_KEY}`,
+      accoutns: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  solidity: "0.8.4",
+};
