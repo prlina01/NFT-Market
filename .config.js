e851efaedeb83159eee0e80838b6d87195c464f2
prlina01 <36077702+prlina01@users.noreply.github.com>
2022-04-16 01:09:20 +0200

    Add an integration test; Add configuration files needed for deployment.

--- .config.js
+++ .config.js
@@ -1,2 +1,2 @@
-export const nftaddress = "0x38F8Be822E2931666948B25B5E396F0DD041867F"
-export const nftmarketaddress = "0xc8Ea70d4d4aEA1BE6601bb785872845b55F7B480"
+export const nftaddress = "0xD41aa46C14e435dF393075032D6BC4Ff1120b99F"
+export const nftmarketaddress = "0x09efFDd9F9906f82fb5FAf3DA86a3221fcdd8d7f"
--- test/test-nft.ts
+++ test/test-nft.ts
@@ -1,32 +1,51 @@
 import {ethers} from "hardhat"
+import { NFT, NFTMarket } from "../typechain";
+import { BigNumber } from "ethers";
+import { expect } from "chai";
 
 describe("NFTMarket", function() {
-  it("Should create and execute market sales", async function() {
+
+  let market: NFTMarket
+  let nft: NFT
+  let nftContractAddress: string
+  let listingPrice: number | BigNumber | string
+  let auctionPrice: BigNumber
+
+  beforeEach(async () => {
     const Market = await ethers.getContractFactory("NFTMarket")
-    const market = await Market.deploy()
+    market = await Market.deploy()
     await market.deployed()
     const marketAddress = market.address
-
     const NFT = await ethers.getContractFactory("NFT")
-    const nft = await NFT.deploy(marketAddress)
+    nft = await NFT.deploy(marketAddress)
     await nft.deployed()
-    const nftContractAddress = nft.address
-
-    let listingPrice = await market.getListingPrice()
-    let listingPrice_1 = listingPrice.toString()
-
-    const auctionPrice = ethers.utils.parseUnits('1', 'ether')
+    nftContractAddress = nft.address
+    listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+    auctionPrice = ethers.utils.parseUnits('1', 'ether')
 
     await nft.createToken("https://location1.com")
     await nft.createToken("https://location2.com")
+  })
+  it("Should create and execute market sales", async function() {
 
-    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice_1 })
-    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice_1 })
 
-    const [_, buyerAddress] = await ethers.getSigners()
+    const [sellerAddress, buyerAddress] = await ethers.getSigners()
+
+
+    await market.connect(sellerAddress).createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
+    await market.connect(sellerAddress).createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+    let num = await market.connect(sellerAddress).fetchItemsCreated()
+    expect(num.length.toString()).to.equal('2')
 
     await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
+    num = await market.connect(buyerAddress).fetchMyNFTs()
+    expect(num.length.toString()).to.equal('1')
 
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 2, { value: auctionPrice})
+
+    num = await market.connect(buyerAddress).fetchMyNFTs()
+    expect(num.length.toString()).to.equal('2')
 
     let items = await market.fetchMarketItems()
     let items_1 = await Promise.all(items.map(async i => {
@@ -40,6 +59,7 @@ describe("NFTMarket", function() {
       }
       return item
     }))
+    expect(items_1.length.toString()).to.equal('0')
     console.log('items: ', items_1)
   })
 })
